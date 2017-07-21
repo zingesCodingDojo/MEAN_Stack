b47194e7a72378051dac104ba275135e93de3e3d
Carlos <carlosgutz25@gmail.com>
2017-07-20 19:54:22 -0700

    Registration

---
+++ Component-Events/Registration/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  user = new User();
+  users = [];
+  onSubmit() {
+    this.users.push(this.user);
+    this.user = new User();
+  }
+}
